83703ad50a07e437c7165e4d69a81d7b164a07cf
yochidros <mm9.movement.trb@gmail.com>
2017-12-22 14:29:18 +0900

    rename Kintai to Embossing

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yochio/yochio/python/Kintai"
+set -gx VIRTUAL_ENV "/Users/yochio/yochio/Embossing"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(Kintai) "            
-            printf "%s%s" "(Kintai) " (set_color normal)
+        if test -n "(Embossing) "            
+            printf "%s%s" "(Embossing) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
